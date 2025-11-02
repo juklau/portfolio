7b7c48b37db9d260f256c2a46046b91be7cb91bd
Klaudia <juklau83@gmail.com>
2025-11-02 22:16:26 +0100

    ajout de RP MédiaStock

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,41 +2,72 @@
 // /*                       Carousel sur la page de Projets*/
 // /* ======================================================================================== */
 
-const pageActuelle = window.location.pathname.includes("ppe-gestion_contacts.html") || window.location.pathname.includes("ppe-chat.html");
-const breakpointsConfig = {
-    0: {
-        slidesPerView: 1
-    },
-    900: {
-        slidesPerView: 2
-    },
-    1200: {
-        slidesPerView: pageActuelle ? 2 : 3
-    }
-};
+document.addEventListener("DOMContentLoaded", () => {
 
-new Swiper('.slider-wrapper', {
-   
-    loop: true,
-    spaceBetween: 15,
-  
-    // Pagination bullets
-    //pagination = tourner la page
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      //afin d'avoir quelques puces visible "dynamiquement"
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
 
+    const pageActuelle = window.location.pathname.includes("ppe-gestion_contacts.html") || 
+                        window.location.pathname.includes("ppe-chat.html") || 
+                        window.location.pathname.includes("ppe-mediastock.html");
+
+    // const isProjetsPage = window.location.pathname.includes("projets.html");
+
+    // function isVisibleSurEcran(element) {
+    //     const rect = element.getBoundingClientRect();
+    //     return (
+    //         rect.top >= 0 &&
+    //         rect.left >= 0 &&
+    //         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    //         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    //     );
+    // }
+
+
+    const breakpointsConfig = {
+        0: {
+            slidesPerView: 1
+        },
+        900: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: pageActuelle ? 2 : 3
+        }
+    };
+
+
+    // // vérifier sur le projet.html is #mes-rp est visible
+    // if (isProjetsPage) {
+    // const mesRpSection = document.getElementById("mes-rp");
     
-    //responsive breakpoints
-    //selon la taille d'écran combien outil-item va s'afficher
-    breakpoints: breakpointsConfig
+    // if (mesRpSection && isVisibleSurEcran(mesRpSection)) {
+    //     breakpointsConfig[1200].slidesPerView = 2;
+    //     }
+    // }
+
+    new Swiper('.slider-wrapper', {
+    
+        loop: true,
+        spaceBetween: 15,
+    
+        // Pagination bullets
+        //pagination = tourner la page
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        //afin d'avoir quelques puces visible "dynamiquement"
+        dynamicBullets: true,
+        },
+    
+        // Navigation arrows
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+
+        
+        //responsive breakpoints
+        //selon la taille d'écran combien outil-item va s'afficher
+        breakpoints: breakpointsConfig
+    });
+
 });
