3360e367d916757519b347a5e43a00dec7dc70d2
Klaudia <juklau83@gmail.com>
2026-04-20 22:33:43 +0200

    docs: mise à jour des applications professionnelles

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -221,7 +221,7 @@ AOS.init({
 /*                          ZOOM Image en plein d'écran
 /* ======================================================================================== */
 
-document.querySelectorAll(".Chat-image, #user-list, .CRM-image, .linkstream-image, #gantt-linkstream, .MediaStock-image, #gantt-classcord, #gantt-mini-crm, .swantrad-image, #gantt-swantrad, #gantt-mediastock, #gantt-roombooking, #gantt-cryptovault, .cryptovault-image, .cryptovault-image-web, .cryptovault-image-web2, .Meteo-image, .RoomBooking-image").forEach(img => {
+document.querySelectorAll(".Chat-image, #user-list, .CRM-image, .linkstream-image, #gantt-linkstream, .MediaStock-image, #gantt-classcord, #gantt-mini-crm, .swantrad-image, #gantt-swantrad, #gantt-mediastock, #gantt-meteo, #gantt-roombooking, #gantt-cryptovault, .cryptovault-image, .cryptovault-image-web, .cryptovault-image-web2, .Meteo-image, .RoomBooking-image").forEach(img => {
   img.addEventListener("click", function () {
 
     //désactiver le zoom x < sm
